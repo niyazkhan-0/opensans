8405ca58042a3ef9406f573e7407c7cfbfc9facc
niyazkhan-0 <niyazkhan987654321@gmail.com>
2026-08-04 20:13:16 +0530

    starting the development of smart service

---
+++ SmartService/src/main/java/com/example/SmartService/SmartServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.SmartService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartServiceApplication.class, args);
+	}
+
+}
---
+++ SmartService/src/test/java/com/example/SmartService/SmartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SmartService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
